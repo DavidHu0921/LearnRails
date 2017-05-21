84ac879776a400f8c72effec64a69f5e982b44c9
David Hu <davidhu920921@gmail.com>
2017-05-22 00:03:20 +0800

    add test

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-        assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
@@ -29,7 +29,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  
+
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
