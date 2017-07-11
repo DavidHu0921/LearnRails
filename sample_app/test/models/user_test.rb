9128fbd6e5dbfeeeaffafa7ddc3ae3f95085df67
David Hu <david.hu@cootek.cn>
2017-07-11 19:00:34 +0800

    add test case

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -8,4 +8,9 @@ class UserTest < ActiveSupport::TestCase
 	test "should be valid" do
 		assert @user.valid?
 	end
+
+	test "name should be present" do 
+		@user.name = "      " 
+		assert_not @user.valid?
+	end
 end
