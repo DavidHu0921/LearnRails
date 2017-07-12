4ec779c91308eda7ef9135834579bfcaa91a0f44
David Hu <david.hu@cootek.cn>
2017-07-12 18:16:42 +0800

    add user email index

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[a-z0-9+\-._]+@[a-z0-9\-._]+(.[a-z0-9\-._]{2,3})+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, 
 										format: { with: VALID_EMAIL_REGEX },
-										uniqueness: true
+										uniqueness: { case_sensitive: false }
 end
