a6ca10dc6425e1469ea117fe878ae0e8c4a035bf
David Hu <david.hu@cootek.cn>
2017-07-12 17:45:06 +0800

    add regular email

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-	validates :name, presence: true
-	validates :email, presence: true
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[a-z0-9+\-._]+@[a-z0-9\-._]+(.[a-z0-9\-._]{2,3})+\z/i
+	validates :email, presence: true, length: { maximum: 255 }, 
+										format: { with: VALID_EMAIL_REGEX },
+										uniqueness: true
 end
--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -18,4 +18,30 @@ class UserTest < ActiveSupport::TestCase
 		@user.email = "     " 
 		assert_not @user.valid?
 	end
+
+	test "name should not be too long" do 
+		@user.name = "a" * 51
+		assert_not @user.valid?
+	end
+
+	test "email should not be too long" do 
+		@user.email = "a" * 244 + "@example.com" 
+		assert_not @user.valid?
+	end
+
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                             first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_address|
+			@user.email = valid_address
+			assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end 
+	end
+
+	test "email addresses should be unique" do 
+		duplicate_user = @user.dup
+		@user.save
+		assert_not duplicate_user.valid?
+	end
+
 end
