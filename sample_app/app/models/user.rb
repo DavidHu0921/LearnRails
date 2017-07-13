bbb87fecff05081d3b1e2f4b20ab58798573457b
David Hu <david.hu@cootek.cn>
2017-07-13 15:10:01 +0800

    add password

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[a-z0-9+\-._]+@[a-z0-9\-._]+(.[a-z0-9\-._]{2,3})+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z0-9\-._]+(.[a-z0-9\-._]{2,3})+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, 
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
+
+	has_secure_password
 end
