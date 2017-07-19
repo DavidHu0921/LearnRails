61a7de89d4c7c97624e63f3efc9f283dbf544cfa
DavidHu0921 <davidhu920921@gmail.com>
2017-07-19 18:55:23 +0800

    add create func

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def create
+		@user = User.new(params[:user]) 			# 不是最终的实现方式
+		if @user.save      
+			# 处理注册成功的情况 
+		else
+			render 'new' 
+		end
+	end
+
 end
