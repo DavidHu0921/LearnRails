2ab3f1ca16e5e919fb5042b84cb3c86e0251a728
DavidHu0921 <davidhu920921@gmail.com>
2017-07-20 16:20:46 +0800

    update email regur

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z0-9\-._]+(.[a-z0-9\-._]{2,3})+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z0-9\-._]+((\.[a-z0-9_-]{2,3}){1,2})+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, 
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
