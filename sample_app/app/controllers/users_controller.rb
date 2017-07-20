71c46a2588401692058fdd809817a51aa28af465
David Hu <davidhu920921@gmail.com>
2017-07-20 23:14:35 +0800

    Finish user signup

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
 		@user = User.new(user_params)		
 		if @user.save      
-			# 处理注册成功的情况 
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
 		else
 			render 'new' 
 		end
