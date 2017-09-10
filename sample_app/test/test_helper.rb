b6440a39b69cd0e8266eb6c0efbeefb3c3a721cb
David Hu <davidhu920921@gmail.com>
2017-09-10 23:57:10 +0800

    add login remember test

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
   require 'rails/test_help'
   require "minitest/reporters"
   Minitest::Reporters.use!
+  
 class ActiveSupport::TestCase
 	fixtures :all
 
@@ -10,4 +11,18 @@ class ActiveSupport::TestCase
 	def is_logged_in?
 		!session[:user_id].nil? 
 	end
+
+	# 登入指定的用户 
+	def log_in_as(user)
+		session[:user_id] = user.id 
+	end
+end
+
+class ActionDispatch::IntegrationTest
+# 登入指定的用户
+	def log_in_as(user, password: 'password', remember_me: '1')
+		post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+	end 
 end
