f39464b639fc6da4fd2a9e91242b83d3cc454ab8
DavidHu0921 <davidhu920921@gmail.com>
2017-10-26 22:48:35 +0800

    add filter

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -37,4 +39,12 @@ class UsersController < ApplicationController
 																	 :password,:password_confirmation)
 		end
 
+		# 前置过滤器
+		# 确保用户已登录 
+		def logged_in_user
+			unless logged_in?
+				flash[:danger] = "Please log in." 
+				redirect_to login_url
+			end
+		end
 end
