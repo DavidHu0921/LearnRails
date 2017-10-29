875db7f8788ed0a0463c4a0d6ef39d78e551bffc
DavidHu0921 <davidhu920921@gmail.com>
2017-10-29 23:59:04 +0800

    add filter

--- sample_app/test/integration/users_edit_test.rb
+++ sample_app/test/integration/users_edit_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
 	def setup
 		@user = users(:michael)
+		@other_user = users(:archer)
 	end
 
 	test "unsuccessful edit" do
@@ -31,5 +32,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
-		end
+	end
+
+
+	test "successful edit with friendly forwarding" do 
+		get edit_user_path(@user)
+		log_in_as(@user)
+		assert_redirected_to edit_user_url(@user)
+		name  = "Foo Bar"
+		email = "foo@bar.com"
+		patch user_path(@user), params: { user: { name:  name,
+			email: email,
+			password:              "",
+			password_confirmation: "" } }
+		assert_not flash.empty?
+		assert_redirected_to @user
+		@user.reload
+		assert_equal name,  @user.name
+		assert_equal email, @user.email
+	end
 end
