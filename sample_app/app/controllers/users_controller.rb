11a4a72f280c71ac41f2c2e4837b4e48e3bac4e1
DavidHu0921 <davidhu920921@gmail.com>
2017-10-30 23:48:44 +0800

    add all users

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:edit, :update]
+	before_action :logged_in_user, only: [:index, :edit, :update]
 	before_action :correct_user,   only: [:edit, :update]
+
+	def index 
+		@users = User.all
+	end
 
 	def show
 		@user = User.find(params[:id])
@@ -44,6 +48,7 @@ class UsersController < ApplicationController
 		# 确保用户已登录 
 		def logged_in_user
 			unless logged_in?
+				store_location
 				flash[:danger] = "Please log in." 
 				redirect_to login_url
 			end
