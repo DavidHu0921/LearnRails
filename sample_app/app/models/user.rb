7fedd5067b172efc6ac080f42381a249b3720a75
DavidHu0921 <davidhu920921@gmail.com>
2017-11-21 19:54:13 +0800

    add column

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
-
-	before_save { self.email = email.downcase }
+	attr_accessor :remember_token, :activation_token 
+	before_save :downcase_email
+	before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z0-9\-._]+((\.[a-z0-9_-]{2,3}){1,2})+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, 
@@ -39,4 +39,15 @@ class User < ApplicationRecord
 		update_attribute(:remember_digest, nil)
 	end
 
+	private
+
+		# 把电子邮件地址转换成小写 
+		def downcase_email
+			self.email = email.downcase 
+		end
+		# 创建并赋值激活令牌和摘要 
+		def create_activation_digest
+			self.activation_token  = User.new_token
+			self.activation_digest = User.digest(activation_token) 
+		end
 end
