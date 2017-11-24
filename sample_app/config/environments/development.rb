5e808d78941c6473224c08f98e44bd9c92ef9e84
DavidHu0921 <davidhu920921@gmail.com>
2017-11-24 16:44:12 +0800

    add mailer

--- sample_app/config/environments/development.rb
+++ sample_app/config/environments/development.rb
@@ -27,7 +27,11 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'  # 不要原封不动使用这个域名，
+                        # 应该使用你本地的开发主机地址 
+  config.action_mailer.default_url_options = { host: 'example.com' }
 
   config.action_mailer.perform_caching = false
 
