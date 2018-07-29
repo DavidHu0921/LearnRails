2665c0812938910e8c64b1170db8ea4337c186c9
DavidHu0921 <davidhu920921@gmail.com>
2018-07-29 18:59:41 +0800

    add following&followed view

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# User
 User.create!(name:  "Example User",
 	email: "example@railstutorial.org",
 	password:              "foobar",
@@ -26,9 +27,22 @@ User.create!(name:  "Example User",
 		activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 	50.times do
 	  content = Faker::Lorem.sentence(5)
 	  users.each { |user| user.microposts.create!(content: content) }
 end
     
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
+
+
+
+
