f81f65fd0c192e62e231db5d1b3ce1db42262b6f
DavidHu0921 <davidhu920921@gmail.com>
2018-07-29 17:03:42 +0800

    fix micropost interface test

--- sample_app/test/integration/microposts_interface_test.rb
+++ sample_app/test/integration/microposts_interface_test.rb
@@ -9,27 +9,28 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-    assert_select 'input[type=FILL_IN]'
     # 无效提交
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } } 
     end
     assert_select 'div#error_explanation'
+
     # 有效提交
-    content = "This micropost really ties the room together"
-    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png') 
+    content = "This micropost really ties the room together" 
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content, picture: FILL_IN } 
+      post microposts_path, params: { micropost: { content: content } } 
     end
-    assert FILL_IN.picture?
+    assert_redirected_to root_url 
     follow_redirect!
     assert_match content, response.body 
+
     # 删除一篇微博
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first 
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost) 
     end
+    
     # 访问另一个用户的资料页面(没有删除链接) 
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
