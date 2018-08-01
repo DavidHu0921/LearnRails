af9eb869636e09ecdc6a8f72f443167408d13c75
DavidHu0921 <davidhu920921@gmail.com>
2018-08-01 23:28:26 +0800

    add follow&unfollow pages

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   end
 	resources :users
 	resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
